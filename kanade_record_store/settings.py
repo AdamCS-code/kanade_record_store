eb8ed663a1842962ed58e01235f4464b099a1409
Adam <adamsembiring01@gmail.com>
2024-09-24 18:04:18 +0700

    Mengerjakan Tugas 4 (-Readme.md)

--- kanade_record_store/settings.py
+++ kanade_record_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7y8!u3cyd9ws((3@ptr56k&(bfm@1e2*&pl(+#^pjkb^bau&2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "adam-caldipawell-kanaderecordstore.pbp.cs.ui.ac.id"]
 
--- main/views.py
+++ main/views.py
@@ -1,9 +1,49 @@
-
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect
 from main.forms import ItemEntryForm
 from main.models import Item
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def login_user(request):
+   if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
 
 def show_json(request):
     data = Item.objects.all()
@@ -25,17 +65,22 @@ def create_item_entry(request):
     form = ItemEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        item_entry = form.save(commit=False)
+        item_entry.user = request.user
+        item_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
     return render(request, "create_item_entry.html", context)
 
+@login_required(login_url='/login')
 def show_main(request):
-    items = Item.objects.all()
+    items = Item.objects.filter(user=request.user)
     context = {
-        'name' : 'Adam Caldipawell Sembiring',
-        'class' : 'PBP F',
-        'items' : items,
+        'name': request.user.username,
+        'class': 'PBP F',
+        'npm': '2306227160',
+        'items': items,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
