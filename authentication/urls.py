cc74225741078554a39c9c713f0b58446e4845c4
Adam <adamsembiring01@gmail.com>
2024-12-26 10:36:01 +0700

    add functionality for flutter

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from authentication.views import login
+from authentication.views import login, register, create_product_flutter, logout, show_json
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+    path('logout/', logout, name='logout'),
+    path('show_json/', show_json),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import authenticate, login as auth_login
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from django.contrib.auth import logout as auth_logout
+from django.core import serializers
 import json
 
+from main.models import Item
 @csrf_exempt
 def login(request):
     username = request.POST['username']
@@ -68,3 +71,42 @@ def register(request):
             "status": False,
             "message": "Invalid request method."
         }, status=400)
+    
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_product = Item.objects.create(
+            user=request.user,
+            name=data["name"],
+            price=int(data["price"]),
+            description=data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
